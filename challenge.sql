0c373f027d98a0cae8d0061e10e1e046071efe61
Luv Patel <luvp98@gmail.com>
2026-04-18 18:52:49 -0400

    Completed Task One

---
+++ challenge.sql
@@ -0,0 +1,11 @@
+-- Task 1: Identifying the five customers with the highest lifetme spend
+SELECT 
+    c.first_name || ' ' || c.last_name AS customer_full_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
+
